43283fc20342a1fc75f4aec5abe90010cfd04645
saveplanet <vivek.vbansal@gmail.com>
2022-02-25 16:04:30 +0800

    removed the demo files. working with axios now

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,30 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ducks } from './demo';
-import DuckItem from './DuckItem';
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+      axios.get('http://localhost:5000/api/activities').then(response=>{
+        console.log(response);
+        setActivities(response.data);
+      }
+      )
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {ducks.map(duck => (
-          <div key={duck.name}>
-          <DuckItem props={duck}></DuckItem>
-          </div>
-        ))}
-        <p style={{color:'red'}}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {activities.map((activity: any) => {
+            <div key={activity.id}></div>
+          })}
+        </ul>
       </header>
     </div>
   );
